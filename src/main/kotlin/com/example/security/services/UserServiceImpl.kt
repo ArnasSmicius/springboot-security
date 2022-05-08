127fbe27a7a899f6e14a8ef13639cf76809b0046
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2022-05-08 12:39:07 +0300

    Implement Slf4j logging for UserService

--- src/main/kotlin/com/example/security/services/UserServiceImpl.kt
+++ src/main/kotlin/com/example/security/services/UserServiceImpl.kt
@@ -15,24 +15,29 @@ class UserServiceImpl(
     private val roleRepository: RoleRepository,
 ) : UserService {
     override fun saveUser(user: User): User {
+        log.info("Saving new user ${user.name} to the database")
         return userRepository.save(user)
     }
 
     override fun saveRole(role: Role): Role {
+        log.info("Saving new role ${role.name} to the database")
         return roleRepository.save(role)
     }
 
     override fun addRoleToUser(username: String, roleName: String) {
+        log.info("Adding role $roleName to user $username")
         val user = userRepository.findByUsername(username)
         val role = roleRepository.findByName(roleName)
         user.roles.add(role)
     }
 
     override fun getUser(username: String): User {
+        log.info("Fetching user $username")
         return userRepository.findByUsername(username)
     }
 
     override fun getUsers(): List<User> {
+        log.info("Fetching all the users")
         return userRepository.findAll()
     }
 
