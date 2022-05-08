c63a159ff324e5ee00dc94c83978c3bc70825797
Henrikas Arnas Smičius <henrikas.smicius@vinted.com>
2022-05-08 16:41:33 +0300

    Implement JWT access and refresh tokens

--- src/main/kotlin/com/example/security/filter/CustomAuthenticationFilter.kt
+++ src/main/kotlin/com/example/security/filter/CustomAuthenticationFilter.kt
@@ -1,10 +1,14 @@
 package com.example.security.filter
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import java.util.Date
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -21,12 +25,27 @@ class CustomAuthenticationFilter(
     }
 
     override fun successfulAuthentication(
-        request: HttpServletRequest?,
-        response: HttpServletResponse?,
+        request: HttpServletRequest,
+        response: HttpServletResponse,
         chain: FilterChain?,
-        authResult: Authentication?
+        authResult: Authentication
     ) {
-        super.successfulAuthentication(request, response, chain, authResult)
+        val user = authResult.principal as User
+        val algorithm = Algorithm.HMAC256("secret".toByteArray())
+        val accessToken = JWT.create()
+            .withSubject(user.username)
+            .withExpiresAt(Date(System.currentTimeMillis() + 10 * 60 * 1000))
+            .withIssuer(request.requestURL.toString())
+            .withClaim("roles", user.authorities.map { it.authority })
+            .sign(algorithm)
+        val refreshToken = JWT.create()
+            .withSubject(user.username)
+            .withExpiresAt(Date(System.currentTimeMillis() + 30 * 60 * 1000))
+            .withIssuer(request.requestURL.toString())
+            .sign(algorithm)
+
+        response.setHeader("access_token", accessToken)
+        response.setHeader("refresh_token", refreshToken)
     }
 
     companion object {
